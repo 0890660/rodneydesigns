12ebc427ddf6a1adf833f9cdb75c05fd70d049e8
0890660 <r-vdberg95@hotmail.com>
2022-08-26 07:56:58 +0200

    Add files via upload

--- rodneydesigns/script.js
+++ rodneydesigns/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var maincontent1 = document.createElement("section");
+var contentBlock = document.createElement("div");
+
+maincontent1.className = `maincontent`;
+document.body.appendChild(maincontent1);
+
+var createContent = function (amount) {
+  var currentContentBlock = document.createElement("div");
+  maincontent1.className = `contentblock`;
+  document.maincontent.appendChild(maincontent1);
+};
+
+createContent();
